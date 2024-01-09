be53a153001a2db9199ee13b75cdd815a120a8b3
Boris Balligand <advent_children@hotmail.fr>
2024-01-09 14:53:14 +0100

    add features login

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
-const gallery = document.querySelector(".gallery")
-const filters = document.querySelector(".filters")
+const gallery = document.querySelector(".gallery");
+const filters = document.querySelector(".filters");
 
+
+console.log(gallery)
+console.log(filters)
 async function getWorks() {
     const response = await fetch("http://localhost:5678/api/works");
     return await response.json();
@@ -8,16 +11,16 @@ async function getWorks() {
 
 
 async function creatework() {
-    const arrayWorks = await getWorks()
+    const arrayWorks = await getWorks();
     arrayWorks.forEach(element => {
-    const img = document.createElement("img")
-    const figure = document.createElement("figure")
-    const figcaption = document.createElement("figcaption")
-    img.src = element.imageUrl
-    figcaption.textContent = element.title
-    figure.appendChild(img)
-    figure.appendChild(figcaption)
-    gallery.appendChild(figure)
+    const img = document.createElement("img");
+    const figure = document.createElement("figure");
+    const figcaption = document.createElement("figcaption");
+    img.src = element.imageUrl;
+    figcaption.textContent = element.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
     });
 }
 
@@ -27,47 +30,45 @@ creatework()
 //*************catégories****************//
 
 async function getCategories() {
-    const response = await fetch("http://localhost:5678/api/categories")
-    return await response.json()
-}
+    const response = await fetch("http://localhost:5678/api/categories");
+    return await response.json();
+};
 
 
 async function displayCategoriesButtons() {
-    const categories = await getCategories()
+    const categories = await getCategories();
     
     categories.forEach((category) => {
-        const btn = document.createElement("button")
-        btn.textContent = category.name
-        btn.id = category.id
-        filters.appendChild(btn)
-    })
+        const btn = document.createElement("button");
+        btn.textContent = category.name;
+        btn.id = category.id;
+        filters.appendChild(btn);
+        
+    });
 }
 displayCategoriesButtons()
 
 //filters categories click//
 
 async function categoriesFilterClick(){
-    const book = await getWorks()
-    
-    const buttons = document.querySelectorAll(".filters button")
-    
+    const book = await getWorks();
+    const buttons = document.querySelectorAll(".filters button");
+    console.log(buttons)
     buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        const btnId = e.target.id
-        gallery.innerHTML = ""
-        if (btnId !== "0") {
+        ClickId = e.target.id;
+        console.log(btnId);
+        gallery.innerHTML = "";
+        if (ClickId !== "0") {
             const bookCategoryFilter = book.filter((work) => {
-                return work.categoryId == btnId
+                return work.categoryId == ClickId;
             })
             bookCategoryFilter.forEach((work) => {
-                creatework(work)
+                creatework(work);
             });
-        } else {
-            
         }
-        
-    })
-})
+    });
+});
 
 }
 categoriesFilterClick()
