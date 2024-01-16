a4db0511c7ba2f57a2de7c5e22ca426307661f11
Boris Balligand <advent_children@hotmail.fr>
2024-01-16 13:14:26 +0100

    add modal box

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,6 +1,8 @@
 import { formLogin, email, password } from "./domLinker.js";
 import { postLogin } from "./api.js";
 
+const login = false;
+
 formLogin.addEventListener("submit", e => {
   e.preventDefault();
 
@@ -8,6 +10,8 @@ formLogin.addEventListener("submit", e => {
     .then(data => {
       localStorage.token = data.token
       window.location.replace("index.html");
+      const login = true;
     })
     .catch(error => alert(error))
+    const login = false;
 });
---
+++ FrontEnd/scripts/modal.js
@@ -0,0 +1,8 @@
+const closemodal = document.querySelector(".containerModal .fa-xmark");
+
+closemodal.addEventListener("click"), () => {
+    console.log("closemodal")
+    containerModal.style.display = "none";
+}
+
+
