29948b1b1e85701d33af82101cd569ef24fcea25
Boris Balligand <advent_children@hotmail.fr>
2024-01-16 14:56:50 +0100

    add polish

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -12,7 +12,24 @@ formLogin.addEventListener("submit", e => {
       localStorage.token = data.token
       window.location.replace("index.html");
       //const login = true;
+      const element = document.getElementsByClassName
+      element.remove()
     })
     .catch(error => alert(error))
     //const login = false;
 });
+
+
+function getConnexion() {
+  const token = JSON.parse(localStorage.getItem('auth')).token;
+  return 'Bearer ' + token;
+}
+
+function Connected() {
+  const connecting = getConnexion() ? true : false;
+  return connecting;
+}
+
+if (Connected()) {
+  modifier.style.display = "flex";
+}
